2e5029fe3a3cfcd1635af118e84b3b6129a2d317
sakshi-bhujbal <bhujbalsakshi510@gmail.com>
2024-05-20 22:06:37 +0530

    commit

---
+++ getset.js
@@ -0,0 +1,7 @@
+let para = document.querySelector("p");
+console.log(para);
+let value= para.getAttribute("class");
+console.log(value);
+
+let value2= para.setAttribute("class","newClass");
+console.log(value2);
